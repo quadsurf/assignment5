5508d3cc376d8a0036671ec9839a1b7a4c0dc6e3
Christian Castro <castroMBair@castroMBair.local>
2016-05-18 10:04:46 -0500

    Add Proper Data Structure to User Saves

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
-// start image setter
-// bannerImage = document.getElementById('bannerImg');
-// imgData = getBase64Image(bannerImage);
-// localStorage.setItem("imgData", imgData);
-// end image setter
-
 // start base64 image stringifier
-// function getBase64Image(img) {
-//     var canvas = document.createElement("canvas");
-//     canvas.width = img.width;
-//     canvas.height = img.height;
-//
-//     var ctx = canvas.getContext("2d");
-//     ctx.drawImage(img, 0, 0);
-//
-//     var dataURL = canvas.toDataURL("image/png");
-//
-//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-// }
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 // end base64 image stringifier
 
 // start image getter
 // var dataImage = localStorage.getItem('imgData');
-// bannerImg = document.getElementById('tableBanner');
+// bannerImg = document.getElementById('test');
 // bannerImg.src = "data:image/png;base64," + dataImage;
 // end image getter
 
 
-var localstorage = {
+var data = {
     set: function(key, value) {
         window.localStorage.setItem(key, JSON.stringify(value));
     },
@@ -39,68 +33,149 @@ var localstorage = {
     }
 }
 
+var files = {};
+function readFromFileBrowser($el) {
+    var reader = new FileReader();
+    var name = $($el).attr('name');
+
+    reader.readAsDataURL($el[0].files[0]);
+    reader.onload = function (e) {
+      files[name] = e.target.result;
+    };
+}
+
+function updateUser(user) {
+    data.set('user', user);
+    data.set(user.id, user);
+}
+
 $(function() {
 
-  // var passw = $('#pass');
-  // var cbutton = $('#cbutton');
-  // var emaila = $('#email');
-  // if (!passw.is(":focus")) {
-  //   cbutton.hide();
-  //   console.log("pass has no focus");
-  // }
-  // if (emaila.blur()) {
-  //   cbutton.show();
-  //   console.log("pass has focus");
-  // }
+    // var passw = $('#pass');
+    // var cbutton = $('#cbutton');
+    // var emaila = $('#email');
+    // if (!passw.is(":focus")) {
+    //   cbutton.hide();
+    //   console.log("pass has no focus");
+    // }
+    // if (emaila.blur()) {
+    //   cbutton.show();
+    //   console.log("pass has focus");
+    // }
 
-  $('#categoryID').hide();
-  $('#toppingID').hide();
+    $('#categoryID').hide();
+    $('#toppingID').hide();
 
-  $('.form-slider').slick({
-    // setting-name: setting-value
-    // breaks when above line is enabled???
-  });
-
-  // Init
-	this.init = function() {
-		var userId = localstorage.get('activeUser');
-
-		if(userId) { // If logged in
-
-		} else { // if not logged in
-
-		}
-	}
-
-    $('#create').submit(function() {
-    	var firstname = $('#firstname').val();
-    	var email = $('#email').val();
-    	var password = $('#pass').val();
-
-    	// localstorage.set('firstName', firstname);
-    	// localstorage.set('email', email);
-    	// localstorage.set('password', password);
-
-    	var id = localstorage.get('lastId') + 1;
-
-    	localstorage.set(id, {
-    		'firstname' : firstname,
-    		'email' : email,
-    		'password': password
-    	});
-
-    	localstorage.set('lastId', id);
-    	localstorage.set('activeUser', id);
-
-    	// Finding user first name
-    	var userId = localstorage.get('activeUser');
-    	var user = localstorage.get(userId);
-
-    	$('#welcome a').empty().text('Welcome ' + user.firstname);
-
-    	return false;
+    $('.form-slider').slick({
+        // setting-name: setting-value
+        // breaks when above line is enabled???
     });
 
+    // Learn more about Init
+    this.init = function() {
+        var user = data.get('user');
 
+        if (user) { // If logged in
+            console.log('Logged in User: ' + user);
+            $('#form1').hide();
+            $('#welcome a').empty().text('Welcome ' + user.firstname);
+        } else { // if not logged in
+            console.log('Not logged on!');
+        }
+    }
+
+    this.init();
+
+    $('#create').submit(function() {
+        var firstname = $('#firstname').val();
+        var email = $('#email').val();
+        var password = $('#pass').val();
+
+        // data.set('firstName', firstname);
+        // data.set('email', email);
+        // data.set('password', password);
+
+        var id = data.get('lastId') + 1;
+
+        data.set(id, {
+            'id' : id,
+            'firstname': firstname,
+            'email': email,
+            'password': password
+        });
+
+        var user = data.get(id);
+        data.set('lastId', id);
+        data.set('user', user);
+
+        // Finding user first name
+        $('#welcome a').empty().text('Welcome ' + user.firstname);
+        $(".slick-next").click();
+        $('#form1').hide();
+        return false;
+    });
+
+    $('#addcat').click(function() {
+        $('#categoryID').show();
+        $('#toppingID').show();
+    });
+
+    $('#menu').submit(function() {
+        var catName = $('#catname').val();
+        var baseImageData = files.baseimage;
+
+        var user = data.get('user');
+
+        user.categories = user.categories || [];
+
+        var toppingCount = $('.topping').length;
+
+        var toppings = [];
+        for(i = 1; i <= toppingCount; i++) {
+            var toppingName = $('#toppingname'+i).val();
+            var toppingImageData = files['toppingimage'+i];
+
+            toppings.push({
+                name: toppingName,
+                image: toppingImageData
+            });
+        }//end for loop
+
+        user.categories.push({
+            name : catName,
+            image : baseImageData,
+            toppings: toppings
+        });
+
+        updateUser(user);
+        console.log('updated');
+        // data.set('user', user);
+        // var userId = user.id;
+
+        $('.removebuttonstyling').show();
+
+        return false;
+    });
+
+    $('#addtopp').click(function(){
+        var $copy = $(this).prev().clone();
+        var nextId = $('.topping').length+1;
+
+        //learn more about selector context, i.e $('div', $('.topping'));
+        $copy.attr('id', 'toppingID'+nextId);
+        $('.inputText', $copy).val('');
+        $('.inputText', $copy).attr('id', 'toppingname'+nextId);
+        $('.inputText', $copy).attr('name', 'toppingname'+nextId);
+        $('.inputText', $copy).attr('placeholder', 'topping name '+nextId);
+
+        $('.uploader', $copy).attr('id', 'toppingimage'+nextId);
+        $('.uploader', $copy).attr('name', 'toppingimage'+nextId);
+
+        $(this).before($copy);
+    });
+
+    $('.uploader').change(function() {
+        readFromFileBrowser($(this));
+    });
 
 }); //end jQuery
